0eff3a46aeaa5b7d735b8e99ddb67ff699124ac6
alessandro-lo-presti <mitlopresti@hotmail.it>
2021-02-09 15:25:33 +0100

    bonus

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 var app = new Vue({
   el: "#app",
   data: {
-    cd: []
+    cd: [],
+    genre: [],
+    visible: "all"
   },
   mounted() {
     axios.get("https://flynn.boolean.careers/exercises/api/array/music")
     .then((results) => {
-      this.cd = results.data.response;
-    });
 
+      this.cd = results.data.response;
+
+      this.cd.forEach((e) => {
+        if(!this.genre.includes(e.genre)) {
+          this.genre.push(e.genre);
+        }
+      });
+    });
   }
 });
